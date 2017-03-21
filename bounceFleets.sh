3babcd5f192d759bf937c5d0fcac381b47b91300
ksaittis <ksaittis@gmail.com>
2017-03-21 21:00:24 +0000

    Bouncefleets script

--- bounceFleets.sh
+++ bounceFleets.sh
@@ -5,23 +5,49 @@ readarray INSTANCES < ./describe-instances2.txt
 display_array()
 {
     for i in ${!INSTANCES[@]}; do
-        echo -n "${INSTANCES[$i]}"
+        echo -n Index: $i '->' "${INSTANCES[$i]}"
     done
 }
 
 getSpecificIp()
 {
-    :
+    echo ${INSTANCES[$1]} | awk -F'|' '{print $3}'
 }
 
-loop_instances()
+
+restartTomcatSSH()
+{
+    TOMCAT_VERSION=$(ls /etc/init.d | grep tomcat[6-9] )
+    if [[ -n $TOMCAT_VERSION ]]; then
+        TOMCAT_STOP=$(sudo service ${TOMCAT_VERSION} stop)
+        TOMCAT_START=$(sudo service ${TOMCAT_VERSION} start)
+        TOMCAT_STATUS=$(sudo service ${TOMCAT_VERSION} status)
+        echo $TOMCAT_STATUS
+    else
+        echo "Tomcat is not installed on this box"
+    fi
+}
+
+restartBox()
+{
+    TOMCAT_STATUS=$(ssh -o StrictHostKeyChecking=no -i ~/.ssh/${KEY} ${USERNAME}@${instance_ip//[[:space:]]/} 2> /dev/null "$(typeset -f); restartTomcatSSH")
+    echo $instance_ip '->' $TOMCAT_STATUS
+}
+
+# test=$(ssh -o StrictHostKeyChecking=no -i ~/.ssh/ksaittis.pem ec2-user@34.252.103.143 2> /dev/null "$(typeset -f); restartTomcatSSH")
+echo $test
+
+
+
+loop_arguments()
 {
     j=0
     for i in $@; do
-        echo $j: $i
+        instance_ip=$(getSpecificIp $i)
+        echo Arg $j: $instance_ip
         ((j++))
     done
 }
 
-# loop_instances $@
-display_array
+# display_array
+loop_arguments $@
